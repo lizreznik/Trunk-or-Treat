7479825a92b78a457ea7c97e8703190556b74012
lizreznik <525ereznik@frhsd.com>
2024-10-16 10:50:50 -0400

    everything broke

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-<<<<<<< HEAD
 
 $(document).ready(function () {
-    $('#picture img').each(function () {
-        var description = $(this).data('description');
+  $('#picture img').each(function () {
+    var description = $(this).data('description');
 
-        $(this).hover(function () {
-            $('<div class="description">' + description + '</div>').insertAfter($(this)).fadeIn();
-        }, function () {
-            $(this).next('.description').fadeOut().remove();
-        });
+    $(this).hover(function () {
+      $('<div class="description">' + description + '</div>').insertAfter($(this)).fadeIn();
+    }, function () {
+      $(this).next('.description').fadeOut().remove();
     });
-    /* event details */
-    $('.toggleButton').click(function () {
-        $('.hiddenContent').toggle();
-    });
-    /* schedule */
-    $('.event-heading').on('click', function () {
-        $(this).next('.event-details').slideToggle(); // Toggle the next details
-    });
-});
-=======
-$(document).ready(function() {
-    $('#picture img').each(function() {
-      var description = $(this).data('description'); 
-  
-      $(this).hover(function() {
-        $('<div class="description">' + description + '</div>').insertAfter($(this)).fadeIn();
-      }, function() {
-        $(this).next('.description').fadeOut().remove();
-      });
-    });
-    $('.toggleButton').click(function() {
-        $('.hiddenContent').toggle();
-      });
   });
->>>>>>> 595e1201088ef82452dfb98e5a708a7bae3a0dd0
+  /* event details */
+  $('.toggleButton').click(function () {
+    $('.hiddenContent').toggle();
+  });
+  /* schedule */
+  $('.event-heading').on('click', function () {
+    $(this).next('.event-details').slideToggle(); // Toggle the next details
+  });
+});
+$(document).ready(function () {
+  $('#picture img').each(function () {
+    var description = $(this).data('description');
+
+    $(this).hover(function () {
+      $('<div class="description">' + description + '</div>').insertAfter($(this)).fadeIn();
+    }, function () {
+      $(this).next('.description').fadeOut().remove();
+    });
+  });
+  $('.toggleButton').click(function () {
+    $('.hiddenContent').toggle();
+  });
+});
