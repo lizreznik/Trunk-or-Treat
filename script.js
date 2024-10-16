bc053901a7f60e32ea026fee51fbca98865e6a3e
lizreznik <525ereznik@frhsd.com>
2024-10-16 11:04:03 -0400

    updated JS

--- script.js
+++ script.js
@@ -18,17 +18,3 @@ $(document).ready(function () {
     $(this).next('.event-details').slideToggle(); // Toggle the next details
   });
 });
-$(document).ready(function () {
-  $('#picture img').each(function () {
-    var description = $(this).data('description');
-
-    $(this).hover(function () {
-      $('<div class="description">' + description + '</div>').insertAfter($(this)).fadeIn();
-    }, function () {
-      $(this).next('.description').fadeOut().remove();
-    });
-  });
-  $('.toggleButton').click(function () {
-    $('.hiddenContent').toggle();
-  });
-});
